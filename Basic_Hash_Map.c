f7e72c10a8e2dd30084b38b1c126795e55bee2ef
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-10-22 13:37:30 +0530

    Update Basic_Hash_Map.c

--- Basic_Hash_Map.c
+++ Basic_Hash_Map.c
@@ -2,22 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define the maximum size of the hash table
-#define TABLE_SIZE 100
+#define TABLE_SIZE 1000
 
-// Structure for a key-value pair
 typedef struct KeyValue {
     char* key;
     int value;
     struct KeyValue* next;
 } KeyValue;
 
-// Structure for the hash map
 typedef struct HashMap {
     KeyValue* table[TABLE_SIZE];
 } HashMap;
 
-// Hash function to generate an index from a key
 unsigned int hash(const char* key) {
     unsigned int hash = 0;
     while (*key) {
@@ -27,30 +23,34 @@ unsigned int hash(const char* key) {
     return hash % TABLE_SIZE;
 }
 
-// Function to create a new key-value pair
 KeyValue* createKeyValue(const char* key, int value) {
     KeyValue* kv = (KeyValue*)malloc(sizeof(KeyValue));
+    if (!kv) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
     kv->key = strdup(key);
+    if (!kv->key) {
+        perror("Memory allocation failed");
+        free(kv);
+        exit(EXIT_FAILURE);
+    }
     kv->value = value;
     kv->next = NULL;
     return kv;
 }
 
-// Function to insert a key-value pair into the hash map
 void insert(HashMap* map, const char* key, int value) {
     unsigned int index = hash(key);
     KeyValue* newKeyValue = createKeyValue(key, value);
-
-    // Add the new key-value pair to the beginning of the linked list
     newKeyValue->next = map->table[index];
     map->table[index] = newKeyValue;
 }
 
-// Function to look up a value by key in the hash map
 int lookup(HashMap* map, const char* key) {
     unsigned int index = hash(key);
     KeyValue* current = map->table[index];
-    while (current != NULL) {
+    while (current) {
         if (strcmp(current->key, key) == 0) {
             return current->value;
         }
@@ -59,16 +59,14 @@ int lookup(HashMap* map, const char* key) {
     return -1; // Key not found
 }
 
-// Function to delete a key-value pair from the hash map
 void deleteKey(HashMap* map, const char* key) {
     unsigned int index = hash(key);
     KeyValue* current = map->table[index];
     KeyValue* prev = NULL;
 
-    while (current != NULL) {
+    while (current) {
         if (strcmp(current->key, key) == 0) {
             if (prev == NULL) {
-                // If the key to be deleted is the first in the list
                 map->table[index] = current->next;
             } else {
                 prev->next = current->next;
@@ -82,6 +80,31 @@ void deleteKey(HashMap* map, const char* key) {
     }
 }
 
+void displayHashMap(const HashMap* map) {
+    printf("Hash Map Contents:\n");
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        KeyValue* current = map->table[i];
+        if (current) {
+            while (current) {
+                printf("  Key: %s, Value: %d\n", current->key, current->value);
+                current = current->next;
+            }
+        }
+    }
+}
+
+void cleanupHashMap(HashMap* map) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        KeyValue* current = map->table[i];
+        while (current) {
+            KeyValue* temp = current;
+            current = current->next;
+            free(temp->key);
+            free(temp);
+        }
+    }
+}
+
 int main() {
     HashMap map;
     for (int i = 0; i < TABLE_SIZE; i++) {
@@ -90,29 +113,46 @@ int main() {
 
     char key[50];
     int value;
+    int choice;
 
     while (1) {
         printf("Menu:\n");
         printf("1. Insert key-value pair\n");
         printf("2. Look up value by key\n");
         printf("3. Delete key-value pair\n");
-        printf("4. Quit\n");
+        printf("4. Display the hash map\n");
+        printf("5. Quit\n");
         printf("Enter your choice: ");
         
-        int choice;
-        scanf("%d", &choice);
+        if (scanf("%d", &choice) != 1) {
+            fprintf(stderr, "Invalid input. Please enter a valid option.\n");
+            while (getchar() != '\n');  // Clear the input buffer
+            continue;
+        }
 
         switch (choice) {
             case 1:
                 printf("Enter key: ");
-                scanf("%s", key);
+                if (scanf("%s", key) != 1) {
+                    fprintf(stderr, "Invalid input. Please enter a valid key.\n");
+                    while (getchar() != '\n');  // Clear the input buffer
+                    continue;
+                }
                 printf("Enter value: ");
-                scanf("%d", &value);
+                if (scanf("%d", &value) != 1) {
+                    fprintf(stderr, "Invalid input. Please enter a valid value.\n");
+                    while (getchar() != '\n');  // Clear the input buffer
+                    continue;
+                }
                 insert(&map, key, value);
                 break;
             case 2:
                 printf("Enter key to look up its value: ");
-                scanf("%s", key);
+                if (scanf("%s", key) != 1) {
+                    fprintf(stderr, "Invalid input. Please enter a valid key.\n");
+                    while (getchar() != '\n');  // Clear the input buffer
+                    continue;
+                }
                 int result = lookup(&map, key);
                 if (result != -1) {
                     printf("Value for key '%s' is %d\n", key, result);
@@ -122,24 +162,22 @@ int main() {
                 break;
             case 3:
                 printf("Enter key to delete: ");
-                scanf("%s", key);
+                if (scanf("%s", key) != 1) {
+                    fprintf(stderr, "Invalid input. Please enter a valid key.\n");
+                    while (getchar() != '\n');  // Clear the input buffer
+                    continue;
+                }
                 deleteKey(&map, key);
                 printf("Key '%s' deleted from the hash map\n", key);
                 break;
             case 4:
-                // Clean up memory (free dynamically allocated memory)
-                for (int i = 0; i < TABLE_SIZE; i++) {
-                    KeyValue* current = map.table[i];
-                    while (current != NULL) {
-                        KeyValue* temp = current;
-                        current = current->next;
-                        free(temp->key);
-                        free(temp);
-                    }
-                }
+                displayHashMap(&map);
+                break;
+            case 5:
+                cleanupHashMap(&map);
                 return 0;
             default:
-                printf("Invalid choice. Please select a valid option.\n");
+                fprintf(stderr, "Invalid choice. Please select a valid option.\n");
         }
     }
 }
